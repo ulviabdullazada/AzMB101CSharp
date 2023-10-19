8a18889e807ad115e0324d42e58f96f5cdf184e5
u1viii <dezzzgaming@gmail.com>
2023-10-19 08:07:46 +0400

    ref out

--- ValueAndReferenceTypes/ValueAndReferenceTypes/Program.cs
+++ ValueAndReferenceTypes/ValueAndReferenceTypes/Program.cs
@@ -21,12 +21,46 @@
             //    Console.WriteLine("Ramazan " + pendir);
             //}
             int[] arr = { 1, 5, 8, 11, 16, -4, 7, -8, 9, 22 };
-            SwapMinAndMax(arr);
+            SwapMinAndMax(ref arr);
+
+            //SwapMinAndMax(new int[] { 1, 2, 3, 4, 5 });
             foreach (int i in arr)
             {
                 Console.WriteLine(i);
             }
+            //int.TryParse("123", out int result);
+            //Console.WriteLine(result);
+            //int b = 0;
+            //Console.WriteLine(a);
+
+
+
+
+            int[] nums = { 1, 2, 3 };
+            ChangeArrSize(ref nums, 10);
+            Console.WriteLine(nums.Length);
         }
+
+        static void ChangeArrSize(ref int[] arr, int newSize)
+        {
+            int[] newArr = new int[arr.Length+newSize];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                newArr[i] = arr[i];
+            }
+            arr = newArr;
+        }
+
+        static void ChangeA(out int b)
+        {
+            b = 12344;
+            //b = 50;
+        }
+        //static void ChangeA(ref int b)
+        //{
+        //    b = 12344;
+        //    //b = 50;
+        //}
 
         static int MinIndex(int[] array)
         {
@@ -57,22 +91,22 @@
         // a = a + b = 5 + 8 = 13
         // b = a - b = 13 - 8 = 5
         // a = a - b = 13 - 5 = 8
-        static void SwapMinAndMax(int[] array)
+        static void SwapMinAndMax(ref int[] array)
         {
-            int minI = MinIndex(array); 
-            int maxI = MaxIndex(array);
-
-            //minI = maxI + minI;
-            //maxI = minI - maxI;
-            //minI = minI - maxI;
-            int temp = array[minI];
-            array[minI] = array[maxI];
-            array[maxI] = temp;
-
-
+            Swap(ref array[MinIndex(array)], ref array[MaxIndex(array)]);
+            //int temp = array[minI];
+            //array[minI] = array[maxI];
+            //array[maxI] = temp;
+            //-----
             //array[minI] = array[maxI] + array[minI];
             //array[maxI] = array[minI] - array[maxI];
             //array[minI] = array[minI] - array[maxI];
+        }
+        static void Swap(ref int num1, ref int num2)
+        {
+            int temp = num1;
+            num1 = num2;
+            num2 = temp;
         }
     }
 }
