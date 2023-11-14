e7b589eabeb3fff8c545e15d7497e725a7db7e1f
ulvi.abdullazada@code.edu.az <ulvi.abdullazada@code.edu.az>
2023-11-14 07:25:31 +0200

    tasks - 2

--- Paralellism/Program.cs
+++ Paralellism/Program.cs
@@ -7,6 +7,23 @@ namespace Paralellism
         static int eded = 0;
         static object lockObject =new object();
         static bool isStop = false;
+        static readonly IEnumerable<string> links = new string[]
+        {
+            "https://docs.microsoft.com",
+            "https://docs.microsoft.com/aspnet/core",
+            "https://docs.microsoft.com/azure",
+            "https://docs.microsoft.com/azure/devops",
+            "https://docs.microsoft.com/dotnet",
+            "https://docs.microsoft.com/dynamics365",
+            "https://docs.microsoft.com/education",
+            "https://docs.microsoft.com/enterprise-mobility-security",
+            "https://docs.microsoft.com/gaming",
+            "https://docs.microsoft.com/graph",
+            "https://docs.microsoft.com/microsoft-365",
+            "https://docs.microsoft.com/office",
+            "https://docs.microsoft.com/powershell"
+        };
+
         static void Main(string[] args)
         {
             #region Thread
@@ -36,20 +53,34 @@ namespace Paralellism
             //IncreaseAsync().Wait();
             //DecreaseAsync().Wait();
             //Task.WhenAll(decr, incr).When();
-            Task.WhenAll(Task.Run(() =>
-            {
-                for (int i = 0; i < 500; i++)
-                {
-                    Console.WriteLine(i);
-                }
-            }), Task.Run(() =>
-            {
-                for (int i = 500; i < 1000; i++)
-                {
-                    Console.WriteLine(i);
-                }
-            }));
+            //Task.WhenAll(Task.Run(() =>
+            //{
+            //    for (int i = 0; i < 500; i++)
+            //    {
+            //        Console.WriteLine(i);
+            //    }
+            //}), Task.Run(() =>
+            //{
+            //    for (int i = 500; i < 1000; i++)
+            //    {
+            //        Console.WriteLine(i);
+            //    }
+            //}));
             //Console.WriteLine(eded);
+            #endregion
+            #region CPU Bound
+            //Stopwatch sw = Stopwatch.StartNew();
+            ////SeherYemeyi();
+            //SeherYemeyiAsync().Wait();
+            //Console.WriteLine("Yemek " + (float)sw.ElapsedMilliseconds / 1000f + " deqiqe hazirlandi");
+            #endregion
+            #region IO Bound
+            Stopwatch sw = Stopwatch.StartNew();
+            GetData();
+            Console.WriteLine(sw.ElapsedMilliseconds + " non async");
+            sw.Restart();
+            GetDataAsync().Wait();
+            Console.WriteLine(sw.ElapsedMilliseconds + " async");
             #endregion
         }
         #region Thread
@@ -100,6 +131,92 @@ namespace Paralellism
         static async Task DecreaseAsync()
         {
             await Task.Run(Decrease);
+        }
+        #endregion
+        #region CPU Bound
+        static void SeherYemeyi()
+        {
+            Yumurta();
+            Caydan();
+            Sufre();
+        }
+        static void Yumurta()
+        {
+            Console.WriteLine("Qaz yandirildi");
+            Console.WriteLine("Tava qizdirilir");
+            Console.WriteLine("Yumurta qirilir");
+            Console.WriteLine("Yumurta qarisdirilir");
+            Console.WriteLine("Yumurta bishir");
+            Thread.Sleep(5000);
+            Console.WriteLine("Yumurta bishdi");
+        }
+        static void Caydan()
+        {
+            Console.WriteLine("Qaz yandirildi");
+            Console.WriteLine("Caydana su dolduruldu");
+            Console.WriteLine("Caydan qaynamaga qoyuldu");
+            Thread.Sleep(8000);
+            Console.WriteLine("Cay qaynadi");
+        }
+        static void Sufre()
+        {
+            Thread.Sleep(1000);
+            Console.WriteLine("Sufre hazirdir");
+        }
+        static async Task SeherYemeyiAsync()
+        {
+            //await YumurtaAsync();
+            //await CaydanAsync();
+            //await SufreAsync();
+            await Task.WhenAll( CaydanAsync(), YumurtaAsync(), SufreAsync());
+            await Task.Run(() =>
+            {
+                Console.WriteLine("Nush olsun");
+            });
+        }
+        static async Task YumurtaAsync()
+        {
+            Console.WriteLine("Qaz yandirildi");
+            Console.WriteLine("Tava qizdirilir");
+            Console.WriteLine("Yumurta qirilir");
+            Console.WriteLine("Yumurta qarisdirilir");
+            Console.WriteLine("Yumurta bishir");
+            await Task.Delay(5000);
+            Console.WriteLine("Yumurta bishdi");
+        }
+        static async Task CaydanAsync()
+        {
+            Console.WriteLine("Qaz yandirildi");
+            Console.WriteLine("Caydana su dolduruldu");
+            Console.WriteLine("Caydan qaynamaga qoyuldu");
+            await Task.Delay(8000);
+            Console.WriteLine("Cay qaynadi");
+        }
+        static async Task SufreAsync()
+        {
+            await Task.Delay(1000);
+            Console.WriteLine("Sufre hazirdir");
+        }
+        #endregion
+        #region IO Bound
+        static void GetData()
+        {
+            HttpClient client = new HttpClient();
+            foreach (var link in links)
+            {
+                Console.WriteLine(client.GetStringAsync(link).Result.Length);
+            }
+        }
+        static async Task GetDataAsync()
+        {
+            HttpClient client = new HttpClient();
+            ICollection<Task<string>> tasks = new List<Task<string>>();
+            foreach (var link in links)
+            {
+                //await client.GetStringAsync(link);
+                tasks.Add(client.GetStringAsync(link));
+            }
+            await Task.WhenAll(tasks);
         }
         #endregion
     }
